7b03bec53a9fcb19e7968f10e4e254a3408e09d6
MEI YOSHIZAKI <b242848@hiroshima-u.ac.jp>
2026-07-23 10:56:51 +0900

    rpg風にしてみた

--- src/hitblow/hint.py
+++ src/hitblow/hint.py
@@ -13,19 +13,20 @@ def offer_hint(secret, wrong_tries, hint_position):
 
         if answer == "1":
             if hint_position < len(secret):
+                print("スライム「苦戦しているそんなあなたに！今ならなんと1980円！！」")
                 print(
-                    f"💡 ヒント：{hint_position + 1}桁目の数字は"
-                    f"「{secret[hint_position]}」です"
+                    f"ネクストスライムヒント：「{hint_position + 1}桁目の数字は"
+                    f"「{secret[hint_position]}」だよ！」"
                 )
 
                 # 次回は次の桁をヒントとして表示する
                 return hint_position + 1
 
-            print("すべての桁のヒントを表示済みです。")
+            print("スライムから9999ダメージ! 勇者は気絶した。")
             return hint_position
 
         if answer == "2":
-            print("ヒントを表示せずにゲームを続けます。")
+            print("スライム「ヒントがいらないなんてなんて勇敢なんだ！」")
             return hint_position
 
         print("1 または 2 を入力してください。")
